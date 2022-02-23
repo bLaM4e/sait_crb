48936415833e221ad7a68f8539582004044023cf
Dmitry <konnerspz@mail.ru>
2022-02-23 21:21:14 +0300

    add a link on news

--- js/doctors.js
+++ js/doctors.js
@@ -1,13 +1,219 @@
 'use strict';
 
-// 0 - doctorsStac, 1 - doctorsPolV, 2 - doctorsPolD
-const data = [
-    [
+const data = {
+    stac: [
         {
-            lastname: 'Афанасьева',
-            firstName: 'Людмила',
-            patronymic: 'Сергеевна',
-            post: 'Медицинская сестра палатная',
+            lastname: 'БАБИЧ',
+            firstName: 'ОЛЬГА',
+            patronymic: 'БОРИСОВНА',
+            post: 'Врач приемного отделения',
+            specialization: '',
+            certificate: 'до 16.02.2024',
+            category: 'Высшая категория',
+        },
+        {
+            lastname: 'ВЛАДИМИРОВА',
+            firstName: 'ЛЮБОВЬ',
+            patronymic: 'ВАЛЕРЬЕВНА',
+            post: 'Врач приемного отделения',
+            specialization: '',
+            certificate: 'до 16.02.2024',
+            category: 'Без категории',
+        },
+        {
+            lastname: 'ВЯЗНИКОВЦЕВА',
+            firstName: 'ЕЛЕНА',
+            patronymic: 'ЮРЬЕВНА',
+            post: 'Врач приемного отделения',
+            specialization: '',
+            certificate: 'до 16.02.2024',
+            category: '1 разряд (категория)',
+        },
+        {
+            lastname: 'МАТВЕЕВА',
+            firstName: 'ЖАННА',
+            patronymic: 'ЛЕОНИДОВНА',
+            post: 'Врач приемного отделения',
+            specialization: '',
+            certificate: 'до 16.02.2024',
+            category: '1 разряд (категория)',
+        },
+        {
+            lastname: 'ТРУФАНОВА',
+            firstName: 'ИРИНА',
+            patronymic: 'АЛЕКСАНДРОВНА',
+            post: 'Врач приемного отделения',
+            specialization: '',
+            certificate: 'до 16.02.2024',
+            category: '1 разряд (категория)',
+        },
+        {
+            lastname: 'ПЕТРОВ',
+            firstName: 'АЛЕКСАНДР',
+            patronymic: 'ВИКТОРОВИЧ',
+            post: 'Врач-травматолог-ортопед',
+            specialization: '',
+            certificate: 'до 16.02.2024',
+            category: 'Высшая категория',
+        },
+        {
+            lastname: 'ТРИГУБ',
+            firstName: 'НИКОЛАЙ',
+            patronymic: 'АНДРЕЕВИЧ',
+            post: 'Врач-травматолог-ортопед',
+            specialization: '',
+            certificate: 'до 16.02.2024',
+            category: 'Без категории',
+        },
+        {
+            lastname: 'КОРОВУШКИН',
+            firstName: 'ВАЛЕРИЙ',
+            patronymic: 'ГРИГОРЬЕВИЧ',
+            post: 'Заведующий отделением – врач-хирург',
+            specialization: '',
+            certificate: 'до 16.02.2024',
+            category: 'Без категории',
+        },
+        {
+            lastname: 'ЗИНКИН',
+            firstName: 'СЕРГЕЙ',
+            patronymic: 'ГЕННАДЬЕВИЧ',
+            post: 'Врач-хирург',
+            specialization: '',
+            certificate: 'до 16.02.2024',
+            category: 'Без категории',
+        },
+        {
+            lastname: 'КИСЕЛЕВ',
+            firstName: 'ДМИТРИЙ',
+            patronymic: 'ЕВГЕНЬЕВИЧ',
+            post: 'Врач-хирург',
+            specialization: '',
+            certificate: 'до 16.02.2024',
+            category: 'Высшая категория',
+        },
+        {
+            lastname: 'КОРОБОВ',
+            firstName: 'НИКОЛАЙ',
+            patronymic: 'НИКОЛАЕВИЧ',
+            post: 'Заведующий отделением – врач-хирург',
+            specialization: '',
+            certificate: 'до 16.02.2024',
+            category: 'Высшая категория',
+        },
+        {
+            lastname: 'БАЛДЕНКОВ',
+            firstName: 'АЛЕКСАНДР',
+            patronymic: 'НИКОЛАЕВИЧ',
+            post: 'врач-кардиолог',
+            specialization: '',
+            certificate: 'до 16.02.2024',
+            category: 'Высшая категория',
+        },
+        {
+            lastname: 'ЗАХАРОВА',
+            firstName: 'ЕЛЕНА',
+            patronymic: 'НИКОЛАЕВНА',
+            post: 'Заведующий отделением – врач-терапевт',
+            specialization: '',
+            certificate: 'до 16.02.2024',
+            category: 'Высшая категория',
+        },
+        {
+            lastname: 'ЧУПИЛКО',
+            firstName: 'ЮЛИЯ',
+            patronymic: 'АНАТОЛЬЕВНА',
+            post: 'Врач-терапевт',
+            specialization: '',
+            certificate: 'до 16.02.2024',
+            category: 'Без категории',
+        },
+        {
+            lastname: 'ЗАЙЦЕВ',
+            firstName: 'АЛЕКСАНДР',
+            patronymic: 'ИГОРЕВИЧ',
+            post: 'врач-травматолог-ортопед',
+            specialization: '',
+            certificate: 'до 16.02.2024',
+            category: 'Высшая категория',
+        },
+        {
+            lastname: 'КОЧНЕВ',
+            firstName: 'АЛЕКСАНДР',
+            patronymic: 'ЮРЬЕВИЧ',
+            post: 'врач-травматолог-ортопед',
+            specialization: '',
+            certificate: 'до 16.02.2024',
+            category: '1 разряд (категория)',
+        },
+        {
+            lastname: 'ЗОТОВ',
+            firstName: 'АЛЕКСАНДР',
+            patronymic: 'ЛЬВОВИЧ',
+            post: 'Врач-хирург',
+            specialization: '',
+            certificate: 'до 16.02.2024',
+            category: '1 разряд (категория)',
+        },
+        {
+            lastname: 'КИСЕЛЕВ',
+            firstName: 'ДМИТРИЙ',
+            patronymic: 'ЕВГЕНЬЕВИЧ',
+            post: 'Врач-хирург',
+            specialization: '',
+            certificate: 'до 16.02.2024',
+            category: 'Высшая категория',
+        },
+        {
+            lastname: 'ЧУБУКОВ',
+            firstName: 'СЕРГЕЙ',
+            patronymic: 'АДОЛЬФОВИЧ',
+            post: 'Заведующий отделением – врач анестезиолог-реаниматолог',
+            specialization: '',
+            certificate: 'до 16.02.2024',
+            category: 'Высшая категория',
+        },
+        {
+            lastname: 'ГОНЧАРОВ',
+            firstName: 'АЛЕКСАНДР',
+            patronymic: 'СЕРГЕЕВИЧ',
+            post: 'Врач-анестезиолог-реаниматолог',
+            specialization: '',
+            certificate: 'до 16.02.2024',
+            category: '1 разряд (категория)',
+        },
+        {
+            lastname: 'ЗАПАНКОВ',
+            firstName: 'ДЕНИС',
+            patronymic: 'НИКОЛАЕВИЧ',
+            post: 'Врач-анестезиолог-реаниматолог',
+            specialization: '',
+            certificate: 'до 16.02.2024',
+            category: 'Высшая категория',
+        },
+        {
+            lastname: 'ИВАНОВ',
+            firstName: 'МАКСИМ',
+            patronymic: 'ВЯЧЕСЛАВОВИЧ',
+            post: 'Врач-анестезиолог-реаниматолог',
+            specialization: '',
+            certificate: 'до 16.02.2024',
+            category: 'Высшая категория',
+        },
+        {
+            lastname: 'ПРОЦЕНКО',
+            firstName: 'АЛЕКСАНДР',
+            patronymic: 'ЮРЬЕВИЧ',
+            post: 'Врач-анестезиолог-реаниматолог',
+            specialization: '',
+            certificate: 'до 16.02.2024',
+            category: '1 разряд (категория)',
+        },
+        {
+            lastname: 'РЫБАЧКОВА',
+            firstName: 'МАРИЯ',
+            patronymic: 'ЮРЬЕВНА',
+            post: 'Врач-анестезиолог-реаниматолог',
             specialization: '',
             certificate: 'до 16.02.2024',
             category: '2 разряд (категория)',
@@ -23,7 +229,7 @@ const data = [
         },
     ],
 
-    [
+    adult: [
         {
             lastname: 'Травникова',
             firstName: 'Рано',
@@ -42,9 +248,27 @@ const data = [
             certificate: 'до 16.02.2024',
             category: 'Без категории',
         },
+        {
+            lastname: 'Ефимова',
+            firstName: 'Вера',
+            patronymic: 'Евгеньевна',
+            post: 'Заместитель главного врача по клинико-экспертной работе',
+            specialization: 'Неврология',
+            certificate: 'до 16.02.2024',
+            category: 'Без категории',
+        },
+        {
+            lastname: 'Ефимова',
+            firstName: 'Вера',
+            patronymic: 'Евгеньевна',
+            post: 'Заместитель главного врача по клинико-экспертной работе',
+            specialization: 'Неврология',
+            certificate: 'до 16.02.2024',
+            category: 'Без категории',
+        },
     ],
 
-    [
+    children: [
         {
             lastname: 'Круть',
             firstName: 'Алина',
@@ -55,26 +279,139 @@ const data = [
             category: 'Высшая категория',
         },
         {
-            lastname: 'Круть',
-            firstName: 'Алина',
-            patronymic: 'Сергеевна',
-            post: 'Заместитель главного врача по поликлинической работе',
+            lastname: 'ВЕЛИКАНОВА',
+            firstName: 'АННА',
+            patronymic: 'ИГОРЕВНА',
+            post: 'Заведующий отделением – врач-педиатр',
             specialization: 'Педиатрия',
             certificate: 'до 16.02.2024',
-            category: 'Высшая категория',
+            category: '2 разряд (категория)',
         },
+        {
+            lastname: 'САГАНДУКОВА',
+            firstName: 'ЛЮБОВЬ',
+            patronymic: 'АЛЕКСЕЕВНА',
+            post: 'Заведующий отделением – врач-педиатр',
+            specialization: 'Педиатрия',
+            certificate: 'до 16.02.2024',
+            category: '2 разряд (категория)',
+        },
+        {
+            lastname: 'ВЕЛИКАНОВА',
+            firstName: 'АННА',
+            patronymic: 'ИГОРЕВНА',
+            post: 'Заведующий отделением – врач-педиатр',
+            specialization: 'Педиатрия',
+            certificate: 'до 16.02.2024',
+            category: '2 разряд (категория)',
+        },
+        {
+            lastname: 'ВЕЛИКАНОВА',
+            firstName: 'АННА',
+            patronymic: 'ИГОРЕВНА',
+            post: 'Заведующий отделением – врач-педиатр',
+            specialization: 'Педиатрия',
+            certificate: 'до 16.02.2024',
+            category: '2 разряд (категория)',
+        },
+        {
+            lastname: 'ВЕЛИКАНОВА',
+            firstName: 'АННА',
+            patronymic: 'ИГОРЕВНА',
+            post: 'Заведующий отделением – врач-педиатр',
+            specialization: 'Педиатрия',
+            certificate: 'до 16.02.2024',
+            category: '2 разряд (категория)',
+        },
+        {
+            lastname: 'ВЕЛИКАНОВА',
+            firstName: 'АННА',
+            patronymic: 'ИГОРЕВНА',
+            post: 'Заведующий отделением – врач-педиатр',
+            specialization: 'Педиатрия',
+            certificate: 'до 16.02.2024',
+            category: '2 разряд (категория)',
+        },
+        {
+            lastname: 'ВЕЛИКАНОВА',
+            firstName: 'АННА',
+            patronymic: 'ИГОРЕВНА',
+            post: 'Заведующий отделением – врач-педиатр',
+            specialization: 'Педиатрия',
+            certificate: 'до 16.02.2024',
+            category: '2 разряд (категория)',
+        },
+        {
+            lastname: 'ВЕЛИКАНОВА',
+            firstName: 'АННА',
+            patronymic: 'ИГОРЕВНА',
+            post: 'Заведующий отделением – врач-педиатр',
+            specialization: 'Педиатрия',
+            certificate: 'до 16.02.2024',
+            category: '2 разряд (категория)',
+        },
+        {
+            lastname: 'ВЕЛИКАНОВА',
+            firstName: 'АННА',
+            patronymic: 'ИГОРЕВНА',
+            post: 'Заведующий отделением – врач-педиатр',
+            specialization: 'Педиатрия',
+            certificate: 'до 16.02.2024',
+            category: '2 разряд (категория)',
+        },
+        {
+            lastname: 'ВЕЛИКАНОВА',
+            firstName: 'АННА',
+            patronymic: 'ИГОРЕВНА',
+            post: 'Заведующий отделением – врач-педиатр',
+            specialization: 'Педиатрия',
+            certificate: 'до 16.02.2024',
+            category: '2 разряд (категория)',
+        },
+        {
+            lastname: 'ВЕЛИКАНОВА',
+            firstName: 'АННА',
+            patronymic: 'ИГОРЕВНА',
+            post: 'Заведующий отделением – врач-педиатр',
+            specialization: 'Педиатрия',
+            certificate: 'до 16.02.2024',
+            category: '2 разряд (категория)',
+        },
+        {
+            lastname: 'ВЕЛИКАНОВА',
+            firstName: 'АННА',
+            patronymic: 'ИГОРЕВНА',
+            post: 'Заведующий отделением – врач-педиатр',
+            specialization: 'Педиатрия',
+            certificate: 'до 16.02.2024',
+            category: '2 разряд (категория)',
+        },
+        
     ]
-];
+};
 
 const doctors = {
     data,
     doctorsBlock: document.querySelector('.persons'),
+    buildingBlock: document.querySelector('.doctors_building'),
+    buttons: document.querySelectorAll('.building_button'),
+    doctorsCards: '',
+
+    init() {
+        this.render();
+        this.getDoctorsCards();
+        this.buildingSelection();
+    },
+
+    getDoctorsCards() {
+        this.doctorsCards = document.querySelectorAll('.doctors_wrapper');
+    },
 
     render() {
-        for (const el of this.data) {
-            for (const item of el) {
+        for (let el in this.data) {
+            for (const item of this.data[el]) {
                 this.doctorsBlock.insertAdjacentHTML('beforeend', `
-                    <div class="doctors_wrapper">
+                    <div class="doctors_wrapper ${el}">
                         <div class="doctors_item">
                             <div class="doctors_name">
                                 <p><span>${item.lastname}</span> ${item.firstName} ${item.patronymic}</p>
@@ -96,6 +433,36 @@ const doctors = {
 
         
     },
+
+    buildingSelection() {
+        this.buildingBlock.addEventListener('click', e => {
+            if (e.target.tagName !== 'BUTTON') return;
+
+            this.buttonIllumination(e.target);
+
+            this.rerender(e.target.id);
+        });
+    },
+
+    buttonIllumination(button) {
+        for (const el of this.buttons) {
+            el.classList.remove('building_button_active');
+        };
+
+        button.classList.add('building_button_active');
+    },
+
+    rerender(buttonId) {
+        for (const el of this.doctorsCards) {
+            el.classList.remove('doctors_hide');
+
+            if (buttonId === 'all') continue;
+
+            if (el.classList[1] !== buttonId) {
+                el.classList.add('doctors_hide');
+            }
+        }
+    }
 }
 
-doctors.render();
+doctors.init();
--- js/news_list.js
+++ js/news_list.js
@@ -4,6 +4,8 @@ const blockNews = {
     init() {
         const news = document.querySelector('.news');
 
+        this.openBlockHash();
+
         news.addEventListener('click', event => {
             if (event.target.classList[0] !== 'news_title') return;
 
@@ -45,8 +47,6 @@ const blockNews = {
         const newsTitleAll = document.querySelectorAll('.news_active');
         const newsTextAll = document.querySelectorAll('.news_text_open');
 
-        console.log(newsTitleAll)
-
         for (let i = 0; i < newsTitleAll.length; i++) {
             const el = newsTitleAll[i];
             if (event.target === el) continue;
@@ -55,6 +55,21 @@ const blockNews = {
                 el.classList.remove('news_active');
                 this.visibleNo(newsTextAll[i]);
             }
+        }
+    },
+
+    openBlockHash() {
+        const locationHash = document.location.hash;
+        
+        if (locationHash !== '') {
+            const elId = locationHash.split('#')[1];
+            const el = document.getElementById(elId);
+            const elTitle = el.querySelector('.news_title')
+            const elText = el.querySelector('.news_text');
+
+            elTitle.classList.toggle('news_active');
+
+            this.visibleOk(elText);
         }
     },
 }
